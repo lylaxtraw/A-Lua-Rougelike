dd97c5162cab73b6c213bf3e41c1d1e643a104c2
Lyla Alice Estrada Pulido <lylaxtraw@gmail.com>
2026-08-06 08:13:45 -0600

    refactor: modificado texto de derrota

--- scenes/gameover_scene.lua
+++ scenes/gameover_scene.lua
@@ -2,7 +2,7 @@ local GameOverScene = {}
 
 function GameOverScene:draw()
     love.graphics.print("=== GAME OVER ===", 50, 50)
-    love.graphics.print("Your run has ended, but your knowledge remains.", 50, 80)
+    love.graphics.print("The slime consumed you. Wanna try again?", 50, 80)
     
     love.graphics.print("1. Return to Saves Menu", 50, 150)
     love.graphics.print("2. Quit Game", 50, 180)
